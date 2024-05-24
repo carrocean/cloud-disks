662bfd77dcbce528670ac27697043efbf87fa3b8
carrocean <12559203+radish-hi_0@user.noreply.gitee.com>
2024-05-24 21:44:23 +0800

    feat: 调整前端user请求路径

--- cloud-disks-ui/src/api/user.js
+++ cloud-disks-ui/src/api/user.js
@@ -1,10 +1,17 @@
 //导入创建好的axios实例
 import { request } from '@/utils/Request.js';
 
+// 接口配置项
+var prefix = '/api/cloud/disks/user/';
+var managerUrl = {
+    login: prefix + 'login',
+    register: prefix + 'register'
+}
+
 //导出login方法，供其它地方使用
 export function login(data) {
     return request({
-        url: '/login',
+        url: managerUrl.login,
         method: 'post',
         data,
     })
@@ -12,7 +19,7 @@ export function login(data) {
 
 export function register(data) {
     return request({
-        url: '/register',
+        url: managerUrl.register,
         method: 'post',
         data,
     })
