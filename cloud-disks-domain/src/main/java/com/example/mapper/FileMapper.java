0e980377e6d37894c9d102c6b097b0ae68818f33
StudyLoser <2041311394@qq.com>
2024-06-17 22:10:36 +0800

    feat:数据访问层中添加了增加文件、删除文件、文件查询、文件动态查询、回收站手动删除、回收站自动删除、回收站查询、回收站恢复文件方法

--- cloud-disks-domain/src/main/java/com/example/mapper/FileMapper.java
+++ cloud-disks-domain/src/main/java/com/example/mapper/FileMapper.java
@@ -2,24 +2,41 @@ package com.example.mapper;
 
 
 import com.example.entity.FileEntity;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 public interface FileMapper {
+    //--文件功能
+    //文件列表查询
+    List<FileEntity> searchFiles(@Param("userId") long userId,
+                                 @Param("originalName") String originalName,
+                                 @Param("type") String type,
+                                 @Param("path") String path,
+                                 @Param("size") String size,
+                                 @Param("date") String date,
+                                 @Param("isFile") Boolean isFile,
+                                 @Param("isDir") Boolean isDir);
 
-    @Select("select * from file where user_id = #{userId} and has_delete = '0'")
-    List<FileEntity> listFileByUserId(Integer userId);
+    //文件增加
+    void addFile(FileEntity file);
+    //文件查询
+    FileEntity getById(@Param("userId") long userId,@Param("id") long id);
 
-    @Select("select * from file where id = #{fileId}")
-    FileEntity getById(String fileId);
+    //文件删除
+    void deleteFile(@Param("userId")long userId,@Param("id") long id);
 
-    @Insert("INSERT INTO file (user_id, original_name, name, is_file, is_dir, size, path, date, type, viewflag, original_path, parent_id) " +
-            "VALUES (#{userId}, #{originalName}, #{name}, #{isFile}, #{isDir}, #{size}, #{path}, #{date}, #{type}, #{viewflag}, #{originalPath}, #{parentId})")
-    void addFile(FileEntity fileEntity);
 
-    @Update("update file set has_delete='1' where user_id = #{userId} and id = #{fileId}")
-    void deleteById(String userId, long fileId);
+    //回收站功能
+    //删除
+    void deleteFilePermanently(@Param("id") long id);
+
+    void deleteOldFilesFromRecycleBin();
+
+    //查看回收站
+    List<FileEntity> getRecycleBinFiles(@Param("userId") long userId);
+    //恢复
+    void restoreFileFromRecycleBin(@Param("id") long id);
+
+
 }
